c36a0acbd015e701f9e0c4533132c022b1339f55
Michelle Balducci <michelle.balducci@gmail.com>
2023-08-29 12:58:36 -0500

    for Lists

--- src/main/java/MinAndMaxNumber.java
+++ src/main/java/MinAndMaxNumber.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MinAndMaxNumber {
 
@@ -6,33 +8,46 @@ public class MinAndMaxNumber {
 
         int[] arr = {1, 1, 3, 4, 5};
 
-        ArrayList arrayList= new ArrayList(){{ add(1);add(2);add(3);add(4);add(5);}};
+       List<Integer> arr2= new ArrayList(){{ add(1);add(3);add(2);add(4);add(5);}};
 
 
         int sumSmallestNums=0;
         int sumlargestNums=0;
 
 
-        for (int i = 0; i < arr.length-1; i++) {
-            if (arr[i] < arr[i + 1]) {
-                    sumSmallestNums+= arr[i];
-                    sumlargestNums+= arr[i+1];
-            }
+        Collections.sort(arr2);
+        System.out.println(arr2.size());
 
-            if (arr[i] == arr[i + 1]) {
+        for (int i=0; i<arr2.size()-1; i++){
+            sumSmallestNums+=arr2.get(i);
+        };
 
-                if (arr[i]<arr[i+2]){
-                    sumSmallestNums+= arr[i];
-                }
-                if (arr[i]>arr[i+2]){
-                    sumlargestNums+= arr[i];
-                }
-            }
+        System.out.println(arr2.toString());
 
-            }
+        }
 
 
-        System.out.println("largest sum is:" + sumlargestNums);
-        System.out.println("smallest sum is:" + sumSmallestNums);
-    }
+
+//        for (int i = 0; i < arr.length-1; i++) {
+//            if (arr[i] < arr[i + 1]) {
+//                    sumSmallestNums+= arr[i];
+//                    sumlargestNums+= arr[i+1];
+//            }
+//
+//            if (arr[i] == arr[i + 1]) {
+//
+//                if (arr[i]<arr[i+2]){
+//                    sumSmallestNums+= arr[i];
+//                }
+//                if (arr[i]>arr[i+2]){
+//                    sumlargestNums+= arr[i];
+//                }
+//            }
+//
+//            }
+
+
+//        System.out.println("largest sum is:" + sumlargestNums);
+//        System.out.println("smallest sum is:" + sumSmallestNums);
+//    }
 }
